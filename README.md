bb6acadbee2ae8212de916319c37a4fb569da358
Rémi Durieu <remi.durieu@univ-rouen.fr>
2019-11-28 16:53:29 +0100

    Move to GitHub --> new repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Random'O'Bot
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
---
+++ randomOBot.js
@@ -0,0 +1,74 @@
+/* === TODO ===
+ * --- Features
+ * 1. Whenever a guild member joins an audio channel, make the bot joins too,
+ * waits a little, and screams
+ * 2. Add parameters to randMoveMe to specify which guild member to move and how
+ * many times -> new command
+ * 3. Add a help command
+ * */
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require('./config.json')
+
+// Print out a message when the bot is ready
+client.on('ready', () => {
+  console.log('Random\'O\'Bot is ready!')
+})
+
+// Load the commands' prefix from the config file
+const prefix = config.prefix
+
+// Defines the bot's root folder
+const rootFolder = '/home/remi/git-repos/random-o-bot/'
+
+client.on('message', (message) => {
+  // If the message doesn't start with the right prefix, or if it comes from a
+  // bot, there is no need to go further
+  if (!message.content.startsWith(prefix) || message.author.bot) {
+    return
+  }
+
+  /* rob!randMoveMe : move the message's author from the audio channel in
+   * which he is currently connected to, to another randomly selected audio
+   * channel */
+  if (message.content.startsWith(prefix + 'randMoveMe')) {
+    // Get the guild from which the message was sent
+    const guild = client.guilds.find(guild => guild.id === message.guild.id)
+
+    // Get a random member among those currently connected to a voice channel
+    const member = guild.members.filter(member => member.voiceChannel).random()
+
+    // Get a random voice channel
+    const voiceChannel = guild.channels.filter(channel => channel.type === 'voice').random()
+
+    // Move selected member to selected channel
+    member.setVoiceChannel(voiceChannel.id)
+      .then(() => console.log('Moved member ' + member.id + ' to channel ' + voiceChannel.id))
+      .catch(console.error)
+  }
+
+  if (message.content.startsWith(prefix + 'test')) {
+    // test function
+  }
+})
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  // Execute the following code only when a guild member joins a channel
+  if (!newMember.voiceChannel) {
+    return
+  }
+
+  // Get the voice channel the member is connected to
+  const voiceChannel = newMember.voiceChannel
+
+  // Join the voice channel
+  voiceChannel.join()
+    .then(connection => {
+      const broadcast = client.createVoiceBroadCast
+      const dispatcher = connection.playFile(rootFolder + 'audio/MegalovAfterLife 0.1.mp3')
+    })
+    .catch(console.error)
+})
+
+client.login(config.token)
